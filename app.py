1e4afbc3efd18e80ba44ecda5aac05e303a3df8b
michaelfechner <resetknopf@googlemail.com>
2025-06-17 16:33:56 +0200

    added function to update posts, added CSS for Update and Delete Buttons

--- app.py
+++ app.py
@@ -53,6 +53,37 @@ def delete_post(post_id):
 
     return redirect(url_for('index'))
 
+@app.route('/update/<int:post_id>', methods=['GET'])
+def update_form(post_id):
+    posts = load_posts()
+    post = next((p for p in posts if p['id'] == post_id), None)
+
+    if not post:
+        return "Post not found", 404
+
+    return render_template('update.html', post=post)
+
+
+@app.route('/update/<int:post_id>', methods=['POST'])
+def update_post(post_id):
+    posts = load_posts()
+    post = next((p for p in posts if p['id'] == post_id), None)
+
+    if not post:
+        return "Post not found", 404
+
+    # Felder aktualisieren
+    post['author'] = request.form.get('author')
+    post['title'] = request.form.get('title')
+    post['content'] = request.form.get('content')
+
+    # JSON zurückschreiben
+    with open('blog_posts.json', 'w') as f:
+        json.dump(posts, f, indent=4)
+
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
