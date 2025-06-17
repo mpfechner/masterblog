0e936a3e1f1b617954cabd89e5bb13ac4d948dc0
michaelfechner <resetknopf@googlemail.com>
2025-06-17 16:07:41 +0200

    added function to add posts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect, url_for, render_template
 import json
 
 
@@ -14,6 +14,36 @@ def load_posts():
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/add', methods=['GET'])
+def add_form():
+    return render_template('add.html')
+
+
+@app.route('/add', methods=['POST'])
+def add_post():
+    author = request.form.get('author')
+    title = request.form.get('title')
+    content = request.form.get('content')
+
+    posts = load_posts()
+    new_id = max((post['id'] for post in posts), default=0) + 1
+
+    posts.append({
+        'id': new_id,
+        'author': author,
+        'title': title,
+        'content': content
+    })
+
+    with open('blog_posts.json', 'w') as f:
+        json.dump(posts, f, indent=4)
+
+    return redirect(url_for('index'))
+
+
+
 
 
 if __name__ == '__main__':
